4186226abe8b13dc37d3aa4e9755c9f31a2d36c7
Falk B. Schimweg <git@falk.schimweg.de>
2026-04-06 00:28:08 +0200

    add missing API

---
+++ native_fisher_py/python/native_fisher_py/data/__init__.py
@@ -0,0 +1,13 @@
+class Device:
+    None_ = -1
+    MS = 1
+    UV = 2
+    Analog = 3
+    PDA = 4
+    AD = 5
+    Esi = 6
+
+class TrayShape:
+    Unknown = 0
+    Rectangular = 1
+    Circular = 2
