4fe38da94987228a398edea1c5b2e8ff932ef412
Falk B. Schimweg <git@falk.schimweg.de>
2026-04-06 22:38:32 +0200

    chore(Native): Debug trailer header on stderr.

--- native/ThermoNativeReader/NativeApi.cs
+++ native/ThermoNativeReader/NativeApi.cs
@@ -1167,8 +1167,8 @@ namespace ThermoNativeReader
             {
                 var info = _rawFile.GetTrailerExtraHeaderInformation();
                 if (info == null) return 0;
+                foreach (var i in info.Take(5)) Console.Error.WriteLine($"[AOT] Field Label: '{i.Label}' Type: {(int)i.DataType}");
                 var res = string.Join("|", info.Select(x => x.Label + ":::" + (int)x.DataType));
-                Console.WriteLine($"[AOT] Field 1 type name: {info[1].DataType.GetType().FullName} value: {(int)info[1].DataType}");
                 var bytes = System.Text.Encoding.UTF8.GetBytes(res);
                 int count = Math.Min(bytes.Length, bufferSize - 1);
                 for (int i = 0; i < count; i++) buffer[i] = bytes[i];
