a83be21a333de4c662830f136b36c0f951d96a90
Falk B. Schimweg <git@falk.schimweg.de>
2026-06-26 20:58:26 +0200

    stricter linting

--- native_fisher_py/python/native_fisher_py/data/__init__.py
+++ native_fisher_py/python/native_fisher_py/data/__init__.py
@@ -5,49 +5,104 @@ from .classes import *
 
 class DataClass(object):
     @property
-    def auto_sampler_information(self): from . import auto_sampler_information; return auto_sampler_information
+    def auto_sampler_information(self):
+        from . import auto_sampler_information
+        auto_sampler_information
+
     @property
-    def common_core_data_object(self): from . import common_core_data_object; return common_core_data_object
+    def common_core_data_object(self):
+        from . import common_core_data_object
+        common_core_data_object
+
     @property
-    def device(self): from . import device; return device
+    def device(self):
+        from . import device
+        device
+
     @property
-    def error_log_entry(self): from . import error_log_entry; return error_log_entry
+    def error_log_entry(self):
+        from . import error_log_entry
+        error_log_entry
+
     @property
-    def file_error(self): from . import file_error; return file_error
+    def file_error(self):
+        from . import file_error
+        file_error
+
     @property
-    def file_header(self): from . import file_header; return file_header
+    def file_header(self):
+        from . import file_header
+        file_header
+
     @property
-    def file_type(self): from . import file_type; return file_type
+    def file_type(self):
+        from . import file_type
+        file_type
+
     @property
-    def filter_accurate_mass(self): from . import filter_accurate_mass; return filter_accurate_mass
+    def filter_accurate_mass(self):
+        from . import filter_accurate_mass
+        filter_accurate_mass
+
     @property
-    def ft_average_options(self): from . import ft_average_options; return ft_average_options
+    def ft_average_options(self):
+        from . import ft_average_options
+        ft_average_options
+
     @property
-    def peak_options(self): from . import peak_options; return peak_options
+    def peak_options(self):
+        from . import peak_options
+        peak_options
+
     @property
-    def raw_file_classification(self): from . import raw_file_classification; return raw_file_classification
+    def raw_file_classification(self):
+        from . import raw_file_classification
+        raw_file_classification
+
     @property
-    def scan_dependent_details(self): from . import scan_dependent_details; return scan_dependent_details
+    def scan_dependent_details(self):
+        from . import scan_dependent_details
+        scan_dependent_details
+
     @property
-    def scan_event(self): from . import scan_event; return scan_event
+    def scan_event(self):
+        from . import scan_event
+        scan_event
+
     @property
-    def scan_events(self): from . import scan_events; return scan_events
+    def scan_events(self):
+        from . import scan_events
+        scan_events
+
     @property
-    def scan_filter(self): from . import scan_filter; return scan_filter
+    def scan_filter(self):
+        from . import scan_filter
+        scan_filter
+
     @property
-    def sequence_file_writer(self): from . import sequence_file_writer; return sequence_file_writer
+    def sequence_file_writer(self):
+        from . import sequence_file_writer
+        sequence_file_writer
+
     @property
-    def sequence_info(self): from . import sequence_info; return sequence_info
+    def sequence_info(self):
+        from . import sequence_info
+        sequence_info
 
     @property
     def source_fragmentation_info_valid_type(self):
         from . import source_fragmentation_info_valid_type
-        return source_fragmentation_info_valid_type
+        source_fragmentation_info_valid_type
 
     @property
-    def tolerance_units(self): from . import tolerance_units; return tolerance_units
+    def tolerance_units(self):
+        from . import tolerance_units
+        tolerance_units
+
     @property
-    def tray_shape(self): from . import tray_shape; return tray_shape
+    def tray_shape(self):
+        from . import tray_shape
+        tray_shape
 
     # Static members
     Device = Device
@@ -79,9 +134,14 @@ class DataClass(object):
     DataUnits = DataUnits
 
     @property
-    def business(self): from . import business as b; return b
+    def business(self):
+        from . import business as b
+        b
+
     @property
-    def filter_enums(self): from . import filter_enums as f; return f
+    def filter_enums(self):
+        from . import filter_enums as f
+        f
 
 
 # Actually instantiate it as the 'data' member in 'data' module
