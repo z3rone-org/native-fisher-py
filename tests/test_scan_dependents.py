5e78d4320552b1591f3c4fc8e2180958b8ce41c3
Falk B. Schimweg <git@falk.schimweg.de>
2026-06-29 11:32:45 +0200

    lint

--- tests/test_scan_dependents.py
+++ tests/test_scan_dependents.py
@@ -5,11 +5,11 @@ def test_scan_dependents_orbitrap_file(orbitrap_raw_file):
     # MS2_MS1_orbitrap.raw has dependents for Scan 1
     deps = orbitrap_raw_file.get_scan_dependents(1, 2)
     assert deps.raw_file_instrument_type != -1
-    
+
     details = deps.scan_dependent_detail_array
     assert isinstance(details, list)
     assert len(details) > 0
-    
+
     det = details[0]
     assert det.filter_string is None
     assert det.scan_index == 6
