8b9cf36e2ad9e4faced50e3360fac704c93f25c2
Falk B. Schimweg <git@falk.schimweg.de>
2026-07-07 15:53:08 +0200

    update thermo vendor repo

--- build.sh
+++ build.sh
@@ -28,7 +28,6 @@ cd ../..
 # Copy the built library into the python package directory
 # This allows maturin to include it in the wheel
 cp $DYLIB_PATH native_fisher_py/python/native_fisher_py/
-cp vendor/RawFileReader/Libs/NetCore/Net8/Assemblies/*.dll native_fisher_py/python/native_fisher_py/
 
 # 2. Build Python Package
 cd native_fisher_py
